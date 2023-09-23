bb9a7975f74b435a3e0a23ddb70803a54e37a9c4
LEales <102584475+LEales@users.noreply.github.com>
2023-09-23 02:10:11 +0200

    Update ShuffleTest.java

--- Chapter5/ShuffleTest.java
+++ Chapter5/ShuffleTest.java
@@ -5,10 +5,14 @@ import java.util.HashMap;
 public class ShuffleTest {
     static String x, y, z;
     static int n, m;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        x = br.readLine(); y = br.readLine(); z = br.readLine();
-        n = x.length(); m = y.length();
+        x = br.readLine();
+        y = br.readLine();
+        z = br.readLine();
+        n = x.length();
+        m = y.length();
         HashMap<String, Boolean> memo = new HashMap<>();
         System.out.println(solve(memo, 0, 0));
     }
@@ -18,22 +22,28 @@ public class ShuffleTest {
         String key = xi + " " + yi;
         if (memo.containsKey(key)) return memo.get(key);
         if (xi == n) {
-            for (int i = yi+xi; i < m; i++) {
-                if (y.charAt(i) != z.charAt(i)) return false;
+            for (int i = yi + xi; i < m; i++) {
+                if (y.charAt(i) != z.charAt(i)) {
+                    memo.put(key, false);
+                    return false;
+                }
             }
             return true;
         }
         if (yi == m) {
-            for (int i = yi+xi; i < n; i++) {
-                if (x.charAt(i) != z.charAt(i)) return false;
+            for (int i = yi + xi; i < n; i++) {
+                if (x.charAt(i) != z.charAt(i)) {
+                    memo.put(key, false);
+                    return false;
+                }
             }
             return true;
         }
-        char cz = z.charAt(xi+yi);
+        char cz = z.charAt(xi + yi);
         char cx = x.charAt(xi);
         char cy = y.charAt(yi);
         if (cx == cz && cy == cz) {
-            boolean solution = solve(memo,xi + 1, yi) || solve(memo, xi, yi + 1);
+            boolean solution = solve(memo, xi + 1, yi) || solve(memo, xi, yi + 1);
             memo.put(key, solution);
             return solution;
         } else if (cx == cz) {
