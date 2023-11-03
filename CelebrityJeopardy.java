6f1403d9478ddd6cf7b94fa5bcc9f52c040b2c0f
LEales <102584475+LEales@users.noreply.github.com>
2023-11-03 15:25:51 +0100

    Add files via upload

---
+++ CelebrityJeopardy.java
@@ -0,0 +1,14 @@
+package contest;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CelebrityJeopardy {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String in;
+        StringBuilder out = new StringBuilder();
+        while ((in = br.readLine()) != null) out.append(in).append('\n');
+        System.out.print(out);
+    }
+}
